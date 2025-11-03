9af1b865fb2d2d5c04b80225f08243488bd1bd39
github-ci <you@example.com>
2025-11-03 14:20:23 +0800

    update++++++++++++

--- src/GaNetBundle.php
+++ src/GaNetBundle.php
@@ -7,6 +7,7 @@ namespace Tourze\GaNetBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class GaNetBundle extends Bundle implements BundleDependencyInterface
 {
@@ -14,6 +15,7 @@ class GaNetBundle extends Bundle implements BundleDependencyInterface
     {
         return [
             DoctrineBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
